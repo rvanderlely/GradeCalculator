6cfe176c055a5b8a563642dd373c474dda51868d
rvanderlely <101234963+rvanderlely@users.noreply.github.com>
2022-05-03 11:48:07 -0500

    Initial Commit

---
+++ GradeCalculator/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  GradeCalculator
+//
+//  Created by Rachel Vanderlely on 5/3/22.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
